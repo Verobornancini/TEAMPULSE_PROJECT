747227bb87695778134b72e91b3f23eb00452bfe
Verobornancini <50381349+Verobornancini@users.noreply.github.com>
2022-01-06 12:18:04 -0300

    Update of documentation

--- analytics/slack/01_data_cleaning.py
+++ analytics/slack/01_data_cleaning.py
@@ -20,7 +20,6 @@ print('-------------------------------------------------------------')
 print('Inicia el proceso de Limpieza y transformación de datos (#01)')
 print('-------------------------------------------------------------')
 
-# 1) Reading environment variables:
 print('PASO 1: Cargamos las variables de entorno')
 # from the environment
 CLIENT = os.getenv('CLIENT_NAME')
@@ -30,49 +29,42 @@ AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
 # Create s3 object (This is how we access non public buckets and objets, i.e. files)
 s3 = boto3.resource('s3', aws_access_key_id=AWS_ACCESS_KEY_ID, aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
 
-# 2) Download the dataset from the url where it is hosted
 print('PASO 2: Obtenición del dataset de mensajes')
-bucketName = "bitlogic-bot"
-fileName = "bitlogic_slack_historial_de_mensajes.csv"
-#bucketName = "%s-bot" % CLIENT
-#fileName = "%s_slack_historial_de_mensajes.csv" % CLIENT
+bucketName = "%s-bot" % CLIENT
+fileName = "%s_slack_historial_de_mensajes.csv" % CLIENT
 s3.Bucket(bucketName).download_file(fileName, fileName) 
-
 df = pd.read_csv(fileName, index_col=0)
 print('------> Cantidad de mensajes en el historial: ',df.shape)
 
-# 3) Filtering messages
 print('PASO 3: Filtrado de mensajes')
-# Filtering null (NaN) messages
+# Filtro mensajes nulos
 df=df.dropna(subset=['text'])
-# Filtering duplicate messages
+# Filtro duplicados
 df = df.drop_duplicates()
-# After the filtering process, reset the index to avoid future problems
+# Reseteo del index, para prevenir futuros problemas
 df.reset_index(inplace=True,drop=True)
-# Filtering unnecesary messages
+# Filtro mensajes considerados innecesarios
 df= df.drop(df[df['text'].str.contains('se ha unido al canal')].index)
 print('------> Cantidad de mensajes despúes del filtrado: ',df.shape)
 
-# 4) Date type transformation 
 print ("PASO 4: Transformación de datos: timestamp to datetime")
-# The date variable comes in timestamp format (Unix timestamp): Number of seconds since ** 01/01/1970 **
-# Creation of a new variable: datetime 
+# La variable "date" viene en formato "timestamp" (Unix timestamp): Número de segundos desde ** 01/01/1970 **
+# Creación de una nueva variable: datetime 
 date = []
 for i, row in df.iterrows():
     timestamp = row['ts']
     dt_object = datetime.fromtimestamp(timestamp)
     date.append(dt_object)
 df['datetime'] = pd.Series(date)
-# Creation of a new variable to be used as an index
+# Creación de una nueva variable para ser usada como "index"
 df['index_date'] = [d.date() for d in df['datetime']]
-# Split the date and time information into different variables
+# Separo la información de fecha y hora en dos variables diferentes
 df['date'] = [d.date() for d in df['datetime']]
 df['time'] = [d.time() for d in df['datetime']]
 df['month'] = df['datetime'].dt.month
 df['year'] = df.datetime.dt.isocalendar().year
 df["week"] = df.datetime.dt.isocalendar().week
 
-# 5) Message translation
 print ("PASO 5: Traducción de mensajes")
 error_message=""
 number=0
@@ -97,22 +89,21 @@ except Exception as e:
     print(str(number))
     print(str(e))
 
-# Replaced null translations by "A"
+# Remplazo de "null translations" por "A"
 df['translations']=df['translations'].fillna("A")
 translated = df[(df.translations != "A")]
 print('------> Cantidad de mensajes traducidos: ',translated.shape)
 non_translated = df[(df.translations == "A")]
 print('------> Cantidad de mensajes no traducidos: ',non_translated.shape)
-# Filtering duplicate messages
+# Filtro mensajes duplicados
 df = df.drop_duplicates()
 
-# save changes
+# Guardo cambios
 df.to_csv(fileName, sep=',')
 s3.Bucket(bucketName).upload_file(fileName, fileName)
 
-# 6) Sentiment analysis of messages in English
 print('PASO 6: Analisis de sentimiento: Textblob')
-# polarity
+# polaridad
 for i, row in df.iterrows():
     message = row['translations']
     if row['polarity'] == 0 or 0.000000:
@@ -121,7 +112,7 @@ for i, row in df.iterrows():
             df.loc[i,'polarity']=result
         except Exception as e:
             print(e)
-# subjectivity
+# subjectividad
 for i, row in df.iterrows():
     message = row['translations']
     if row['subjectivity'] == 0 or 0.000000:
@@ -130,7 +121,7 @@ for i, row in df.iterrows():
             df.loc[i,'subjectivity']=result
         except Exception as e:
             print("An error occurred")
-# Normalization of polarity and subjectivity indices for future analysis
+# Normalización de los índices de polaridad y subjetividad para futuros análisis
 min_max_scaler = preprocessing.MinMaxScaler()
 column_names_to_normalize = ['polarity', 'subjectivity']
 x = df[column_names_to_normalize].values
@@ -138,7 +129,6 @@ x_scaled = min_max_scaler.fit_transform(x)
 df_temp = pd.DataFrame(x_scaled, columns=column_names_to_normalize, index = df.index)
 df[column_names_to_normalize] = df_temp
 
-# 7) Sentiment analysis of messages in Spanish
 print('PASO 7: Analisis de sentimiento: Pysentimiento')
 analyzer = SentimentAnalyzer()
 for i, row in df.iterrows():
@@ -154,11 +144,8 @@ for i, row in df.iterrows():
         except Exception as e:
             print("------> An error occurred")
 
-# 8) Save changes
 print("PASO 8: Persistimos en S3")
-# Saving the clean and translated dataframe as a .csv file
 df.to_csv(fileName, sep=',')
-# Setting the Persistence of the message history in s3
 s3.Bucket(bucketName).upload_file(fileName, fileName)
 
 print('-------------------------------------------------------------')
